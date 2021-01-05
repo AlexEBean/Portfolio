41552e16d5eb7854dbb244bc2a54e1c72c9175eb
Alex <alexbean95@gmail.com>
2021-01-05 12:23:36 -0700

    included group project links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,13 +9,38 @@ const Home = () => {
         <h5>Web Developer</h5>
       </section>
       <section className = "projects">
-        <div className = "project">
-          <a href = "https://libraris.xyz" className = "link">Library Simulation Website</a>
+      <div className = "project">
+          <a 
+            // href = "https://libraris.xyz" 
+            className = "link">
+              Writer's Block Website
+          </a>
           <br></br>
-          <a href = "https://github.com/AlexEBean/library-personal-project" className = "link">Library Simulation Github</a>
+          <a 
+            href = "https://github.com/Writer-s-Block-dawgs/writers-block" 
+            className = "link">
+              Writer's Block Github
+          </a>
         </div>
         <div className = "project">
-          <a href = "https://docs.google.com/document/d/1IuceaYcbLWkkJ9uwJFR5zJ6Zb10v2eWzTOyD-dMRuR8" className = "link">Resume</a>
+          <a 
+            href = "https://libraris.xyz" 
+            className = "link">
+              Library Simulation Website
+          </a>
+          <br></br>
+          <a 
+            href = "https://github.com/AlexEBean/library-personal-project" 
+            className = "link">
+              Library Simulation Github
+          </a>
+        </div>
+        <div className = "project">
+          <a 
+            href = "https://docs.google.com/document/d/1IuceaYcbLWkkJ9uwJFR5zJ6Zb10v2eWzTOyD-dMRuR8" 
+            className = "link">
+              Resume
+          </a>
         </div>
       </section>
     </div>
